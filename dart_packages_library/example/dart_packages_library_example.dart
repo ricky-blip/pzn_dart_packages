be88ec560025be143de29127b48906fd9d50ebb5
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-18 20:33:08 +0700

    update

---
+++ dart_packages_library/example/dart_packages_library_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_packages_library/dart_packages_library.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ dart_packages_library/lib/dart_packages_library.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_packages_library;
+
+export 'src/dart_packages_library_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
